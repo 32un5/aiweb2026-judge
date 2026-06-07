5d33f330642805e99bf83aa09b3037a1493fef90
32un5 <seungeun.oh04@gmail.com>
2026-06-07 10:09:11 +0900

    feat: 콘솔추가

--- src/main/java/com/aiweb/judge/JudgeApplication.java
+++ src/main/java/com/aiweb/judge/JudgeApplication.java
@@ -2,12 +2,24 @@ package com.aiweb.judge;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.event.EventListener;
 
 @SpringBootApplication
 public class JudgeApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(JudgeApplication.class, args);
+    }
+
+    @EventListener(ApplicationReadyEvent.class)
+    public void onReady() {
+        System.out.println();
+        System.out.println("===========================================");
+        System.out.println("추상적 판사님 준비 완료! 접속해보셈");
+        System.out.println(">>>> http://localhost:8080");
+        System.out.println("===========================================");
+        System.out.println();
     }
 
 }
